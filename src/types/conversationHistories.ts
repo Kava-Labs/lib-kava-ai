fe3288791d804fcfdfb7c0856feb5891546283c9
adamrturman <adamrturman@gmail.com>
2025-04-02 12:29:00 -0500

    refactor: add tests and rename directory

--- src/types/conversationHistories.ts
+++ src/types/conversationHistories.ts
@@ -1,3 +1,9 @@
+import type {
+  ChatCompletionAssistantMessageParam,
+  ChatCompletionMessageParam,
+  ChatCompletionToolMessageParam,
+} from 'openai/resources/index';
+
 export type ConversationHistory = {
   id: string;
   model: string;
@@ -7,3 +13,20 @@ export type ConversationHistory = {
 };
 
 export type ConversationHistories = Record<string, ConversationHistory>;
+
+
+export type ChatMessage =
+  | ChatCompletionMessageParam
+  | ChatCompletionToolMessageParam
+  | ChatCompletionAssistantMessageParam;
+
+//  we need to be able to search all text and sort by time
+export type SearchableChatHistory = {
+  id: string;
+  title: string;
+  messages: ChatMessage[];
+  lastSaved: number;
+};
+
+//  type returned from getSearchableHistory
+export type SearchableChatHistories = Record<string, SearchableChatHistory>;
