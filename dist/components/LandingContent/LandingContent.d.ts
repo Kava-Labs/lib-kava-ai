4cfc01082b88050182d0971e1581cca211e205f5
adamrturman <adamrturman@gmail.com>
2025-04-25 12:12:49 -0500

    feat: use logo in landing content

--- dist/components/LandingContent/LandingContent.d.ts
+++ dist/components/LandingContent/LandingContent.d.ts
@@ -1,7 +1,5 @@
-import { ReactNode } from 'react';
 interface LandingContentProps {
     introText: string;
-    logo: ReactNode;
 }
-export declare const LandingContent: ({ introText, logo }: LandingContentProps) => import("react/jsx-runtime").JSX.Element;
+export declare const LandingContent: ({ introText }: LandingContentProps) => import("react/jsx-runtime").JSX.Element;
 export {};
--- dist/components/LandingContent/LandingContent.js
+++ dist/components/LandingContent/LandingContent.js
@@ -1,5 +1,8 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import styles from './LandingContent.module.css';
-export const LandingContent = ({ introText, logo }) => {
-    return (_jsx("div", { className: styles.landingContent, children: _jsxs("div", { className: styles.logoWrapper, children: [logo, _jsx("h2", { className: styles.introText, children: introText })] }) }));
+import { KavaAILogo } from '../KavaAILogo/KavaAILogo';
+import { useIsMobileLayout } from '../../hooks';
+export const LandingContent = ({ introText }) => {
+    const isMobileLayout = useIsMobileLayout();
+    return (_jsx("div", { className: styles.landingContent, children: _jsxs("div", { className: styles.logoWrapper, children: [_jsx(KavaAILogo, { height: isMobileLayout ? 40 : 50 }), _jsx("h2", { className: styles.introText, children: introText })] }) }));
 };
